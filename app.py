f8eb5e83ae11a150a3c888108dde83e8558bd57d
takashi <yagita.takashi@gmail.com>
2023-02-08 19:24:22 +0900

    first commit

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python3
 import os
-
 import aws_cdk as cdk
+from stacks.run_command_basic import RunCommandBasic
 
-from job_man_with_ssm_sfn_handson.job_man_with_ssm_sfn_handson_stack import JobManWithSsmSfnHandsonStack
+env = cdk.Environment(
+    account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
+    region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]),
+)
 
 
 app = cdk.App()
-JobManWithSsmSfnHandsonStack(app, "JobManWithSsmSfnHandsonStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+RunCommandBasic(app, "JobManWithSsmSfnHandsonStack", env=env)
 
 app.synth()
